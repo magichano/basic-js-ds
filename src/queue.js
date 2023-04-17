95a7652ae418fbdda2630e9115db91689b4f93ee
magichano <antonartamonov1@gmail.com>
2023-04-17 15:28:17 +0300

    feat: add stack queue

--- src/queue.js
+++ src/queue.js
@@ -1,23 +1,12 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { ListNode } = require('../extensions/list-node.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
-
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
 class Queue {
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+     if this.length==0 {
+      this.head=new ListNode(null)
+      list = this.head;
+     }
   }
 
   enqueue(/* value */) {
